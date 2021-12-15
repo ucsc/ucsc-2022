926479708e61be342b7cba291c810d5532913b57
Jason Chafin <jasonchafin@gmail.com>
2021-12-15 07:44:44 -0800

    chore: :construction: Styling elements in theme.json

--- functions.php
+++ functions.php
@@ -12,19 +12,19 @@ if ( ! function_exists( 'ucsc_theme_setup' ) ) :
          * Add default posts and comments RSS feed links to <head>.
          */
         add_theme_support( 'automatic-feed-links' );
- 
+
         /**
          * Enable support for post thumbnails and featured images.
          */
         add_theme_support( 'post-thumbnails' );
- 
+
         add_theme_support( 'editor-styles' );
- 
+
         add_theme_support( 'wp-block-styles' );
     }
 endif;
 add_action( 'after_setup_theme', 'ucsc_theme_setup' );
- 
+
 /**
  * Enqueue theme scripts and styles.
  */
@@ -37,8 +37,8 @@ add_action( 'wp_enqueue_scripts', 'ucsc_theme_scripts' );
  * Enqueue Google Roboto font.
  */
 function ucsc_add_google_fonts() {
- 
-wp_enqueue_style( 'ucsc-google-roboto-font', 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900', false ); 
+
+wp_enqueue_style( 'ucsc-google-roboto-font', 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,600;0,700;0,900;1,300;1,400;1,600;1,700;1,900', false );
 }
- 
+
 add_action( 'wp_enqueue_scripts', 'ucsc_add_google_fonts' );
