dc26e4dba52205385d417ece2fd4e9a6c70f8e78
Jason Chafin <jasonchafin@gmail.com>
2022-01-19 10:04:27 -0800

    feat: :sparkles: Wrote "Last Modified" shortcode to be included in footer. WIP.

--- functions.php
+++ functions.php
@@ -64,3 +64,19 @@ function ucsc_copyright(){
 	return $copyright.$year;
 }
 add_shortcode( 'copyright', 'ucsc_copyright' );
+
+/**
+ * Last Modified shortcode
+ * returns the date of the
+ * last time the page was
+ * updated
+ */
+function ucsc_last_modified(){
+	$ucsc_time = get_the_time('U');
+    $ucsc_modified_time = get_the_modified_time('U');
+	if ($ucsc_modified_time >= $ucsc_time + 86400) {
+
+        return '<p class="last-modified">Last modified: '.the_modified_time('F jS, Y').'</p>';
+        }
+}
+add_shortcode( 'last-modified', 'ucsc_last_modified' );
