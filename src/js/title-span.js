e54ddfee47954adc4bc30e5208e7443015b1f69b
Jason Chafin <jasonchafin@gmail.com>
2022-01-24 15:56:30 -0800

    refactor: :pencil2: add ampersand to list of word in `title-span.js`

--- src/js/title-span.js
+++ src/js/title-span.js
@@ -1,7 +1,7 @@
 (function() {
 var heading = document.querySelector('.main-header .wp-block-site-title a').innerHTML;
 var split = heading.split(" ");
-var words = ["of", "Of", "and", "And", "is", "Is"];
+var words = ["of", "Of", "and", "And", "is", "Is", "&"];
 
 for (var i = 0; i < split.length; i++) {
     for (var j = 0; j < words.length; j++) {
