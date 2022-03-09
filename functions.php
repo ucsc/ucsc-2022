1359204d9855904e0a107d671c072da3999d0163
Jason Chafin <jasonchafin@gmail.com>
2022-03-09 09:42:17 -0800

    refactor: :construction: Pulled blog query out of `home.html` and made it a part. Duplicated it to make a second to play with. Edited `theme.json`

--- functions.php
+++ functions.php
@@ -1,36 +1,36 @@
 <?php
 
-if ( ! function_exists('ucsc_theme_setup') ) :
+if (! function_exists('ucsc_theme_setup') ) :
 
-	function ucsc_theme_setup()
-	{
+    function ucsc_theme_setup()
+    {
 
-		add_theme_support('wp-block-styles');
+        add_theme_support('wp-block-styles');
 
-		add_editor_style('build/style-index.css');
+        add_editor_style('build/style-index.css');
 
-		/**
-		 * Register primary navigation menu location
-		 */
-		register_nav_menus(
-			array(
-				'primary'   => __('Primary Navigation', 'theme-ucsc'),
-			)
-		);
+        /**
+         * Register primary navigation menu location
+         */
+        register_nav_menus(
+            array(
+            'primary'   => __('Primary Navigation', 'theme-ucsc'),
+            )
+        );
 
-		/*
-		* Load additional block styles.
-		*/
-		$styled_blocks = ['button'];
-		foreach ($styled_blocks as $block_name) {
-			$args = array(
-				'handle' => "ucsc-$block_name",
-				'src'    => get_theme_file_uri("src/scss/wp-blocks/$block_name.css"),
-				$args['path'] = get_theme_file_path("src/scss/wp-blocks/$block_name.css"),
-			);
-			wp_enqueue_block_style("core/$block_name", $args);
-		}
-	}
+        /*
+        * Load additional block styles.
+        */
+        $styled_blocks = ['button'];
+        foreach ($styled_blocks as $block_name) {
+            $args = array(
+             'handle' => "ucsc-$block_name",
+             'src'    => get_theme_file_uri("src/scss/wp-blocks/$block_name.css"),
+             $args['path'] = get_theme_file_path("src/scss/wp-blocks/$block_name.css"),
+            );
+            wp_enqueue_block_style("core/$block_name", $args);
+        }
+    }
 endif;
 add_action('after_setup_theme', 'ucsc_theme_setup');
 
@@ -39,8 +39,8 @@ add_action('after_setup_theme', 'ucsc_theme_setup');
  */
 function ucsc_theme_scripts()
 {
-	wp_enqueue_style('ucsc-theme-styles', get_stylesheet_uri());
-	wp_enqueue_style('ucsc-theme-styles-scss', get_template_directory_uri() . '/build/style-index.css');
+    wp_enqueue_style('ucsc-theme-styles', get_stylesheet_uri());
+    wp_enqueue_style('ucsc-theme-styles-scss', get_template_directory_uri() . '/build/style-index.css');
 }
 add_action('wp_enqueue_scripts', 'ucsc_theme_scripts');
 
@@ -49,8 +49,8 @@ add_action('wp_enqueue_scripts', 'ucsc_theme_scripts');
  */
 function ucsc_googleapi_scripts()
 {
-	echo '<link rel="preconnect" href="https://fonts.googleapis.com">';
-	echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>';
+    echo '<link rel="preconnect" href="https://fonts.googleapis.com">';
+    echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>';
 }
 add_action('wp_head', 'ucsc_googleapi_scripts');
 
@@ -60,8 +60,8 @@ add_action('wp_head', 'ucsc_googleapi_scripts');
 
 function ucsc_add_admin_scripts()
 {
-	wp_register_script('ucsc-admin-scripts', get_template_directory_uri() . '/build/theme.js', array(), '', true);
-	wp_enqueue_script('ucsc-admin-scripts');
+    wp_register_script('ucsc-admin-scripts', get_template_directory_uri() . '/build/theme.js', array(), '', true);
+    wp_enqueue_script('ucsc-admin-scripts');
 }
 
 add_action('admin_enqueue_scripts', 'ucsc_add_admin_scripts');
@@ -72,9 +72,9 @@ add_action('admin_enqueue_scripts', 'ucsc_add_admin_scripts');
 
 function ucsc_add_scripts()
 {
-	wp_enqueue_style('ucsc-google-roboto-font', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700|Roboto:100,300,400,500,700&display=swap', false);
-	wp_register_script('ucsc-front', get_template_directory_uri() . '/build/theme.js', array(), '', true);
-	wp_enqueue_script('ucsc-front');
+    wp_enqueue_style('ucsc-google-roboto-font', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700|Roboto:100,300,400,500,700&display=swap', false);
+    wp_register_script('ucsc-front', get_template_directory_uri() . '/build/theme.js', array(), '', true);
+    wp_enqueue_script('ucsc-front');
 }
 
 add_action('wp_enqueue_scripts', 'ucsc_add_scripts');
@@ -86,9 +86,9 @@ add_action('wp_enqueue_scripts', 'ucsc_add_scripts');
  */
 function ucsc_copyright()
 {
-	$copyright = '&#169;';
-	$year = date('Y');
-	return $copyright . $year;
+    $copyright = '&#169;';
+    $year = date('Y');
+    return $copyright . $year;
 }
 add_shortcode('copyright', 'ucsc_copyright');
 
@@ -103,19 +103,19 @@ add_shortcode('copyright', 'ucsc_copyright');
  */
 function ucsc_last_modified_helper()
 {
-	$ucsc_modified_time = get_the_modified_time('U');
-	if ($ucsc_modified_time > 0) {
-		return the_modified_time('F jS, Y');
-	} else {
-		return the_time('F jS, Y');
-	}
+    $ucsc_modified_time = get_the_modified_time('U');
+    if ($ucsc_modified_time > 0) {
+        return the_modified_time('F jS, Y');
+    } else {
+        return the_time('F jS, Y');
+    }
 }
 
 function ucsc_last_modified()
 {
-	ob_start();
-	ucsc_last_modified_helper();
-	return ob_get_clean();
+    ob_start();
+    ucsc_last_modified_helper();
+    return ob_get_clean();
 }
 add_shortcode('last-modified', 'ucsc_last_modified');
 
@@ -130,35 +130,35 @@ add_shortcode('last-modified', 'ucsc_last_modified');
  */
 function ucsc_logo_switch($block_content = '', $block = [])
 {
-	$siteURL = get_site_url();
-	// http://localhost:8888 == www
-	if ($siteURL == '') {
-		if (isset($block['blockName']) && 'core/html' === $block['blockName']) {
-			$html = '';
-			return $html;
-		} else if ((is_front_page() && is_home()) || is_front_page() || is_home()) {
-			if (isset($block['blockName']) && 'core/site-title' === $block['blockName']) {
-				$html = str_replace(
-					$block_content,
-					'<h1>
+    $siteURL = get_site_url();
+    // http://localhost:8888 == www
+    if ($siteURL == '') {
+        if (isset($block['blockName']) && 'core/html' === $block['blockName']) {
+            $html = '';
+            return $html;
+        } else if ((is_front_page() && is_home()) || is_front_page() || is_home()) {
+            if (isset($block['blockName']) && 'core/site-title' === $block['blockName']) {
+                $html = str_replace(
+                    $block_content,
+                    '<h1>
 						<a href="https://www.ucsc.edu" class="mainsite-logo" id="logo">UC Santa Cruz</a></h1> ',
-					$block_content
-				);
-				return $html;
-			}
-		} else {
-			if (isset($block['blockName']) && 'core/site-title' === $block['blockName']) {
-				$html = str_replace(
-					$block_content,
-					'<p>
+                    $block_content
+                );
+                return $html;
+            }
+        } else {
+            if (isset($block['blockName']) && 'core/site-title' === $block['blockName']) {
+                $html = str_replace(
+                    $block_content,
+                    '<p>
 						<a href="https://www.ucsc.edu" class="mainsite-logo" id="logo">UC Santa Cruz</a></p> ',
-					$block_content
-				);
-				return $html;
-			}
-		}
-	}
-	return $block_content;
+                    $block_content
+                );
+                return $html;
+            }
+        }
+    }
+    return $block_content;
 }
 
 
@@ -175,21 +175,21 @@ add_filter('render_block', 'ucsc_logo_switch', 10, 2);
  */
 function ucsc_adjust_structure($block_content = '', $block = [])
 {
-	if (is_front_page()) {
-		// On the home page, return the block as is
-		$html = $block_content;
-		return $html;
-	} else {
-		// On all other pages, the site title becomes `p` and page title becomes `h1`
-		if (isset($block['blockName']) && 'core/site-title' === $block['blockName']) {
-			$html = str_replace(
-				'<h1 ',
-				'<p ',
-				$block_content
-			);
-			return $html;
-		}
-	}
-	return $block_content;
+    if (is_front_page()) {
+        // On the home page, return the block as is
+        $html = $block_content;
+        return $html;
+    } else {
+        // On all other pages, the site title becomes `p` and page title becomes `h1`
+        if (isset($block['blockName']) && 'core/site-title' === $block['blockName']) {
+            $html = str_replace(
+                '<h1 ',
+                '<p ',
+                $block_content
+            );
+            return $html;
+        }
+    }
+    return $block_content;
 }
 add_filter('render_block', 'ucsc_adjust_structure', 10, 2);
