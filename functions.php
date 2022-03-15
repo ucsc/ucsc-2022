5a06a56a40ef7082cbe9ac0ffac8b63766b40868
Jason Chafin <jasonchafin@gmail.com>
2022-03-15 07:56:17 -0700

    refactor: :lipstick: enqueue `site-title.css`, add custom Site Title color, refactor `_header.scss`

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ if (! function_exists('ucsc_theme_setup') ) :
         /*
         * Load additional block styles.
         */
-        $styled_blocks = ['button','post-template', 'post-author'];
+        $styled_blocks = ['button','post-template', 'post-author','site-title'];
         foreach ($styled_blocks as $block_name) {
             $args = array(
              'handle' => "ucsc-$block_name",
