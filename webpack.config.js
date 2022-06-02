cc94cb1cc67fa4b5e262af4c61b269312f4e7693
Jason Chafin <jasonchafin@gmail.com>
2022-06-02 14:32:20 -0700

    fix: :bug: `theme.json` and `webpack.config.js`

--- webpack.config.js
+++ webpack.config.js
@@ -7,45 +7,12 @@ const path = require( 'path' );
  */
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config.js' );
 
-/**
- * Based on this answer from Stack Overflow
- * url: https://stackoverflow.com/questions/35903246/how-to-create-multiple-output-paths-in-webpack-config
- */
-
-var config = {
-	// TODO: Add common Configuration
-	module: {},
-};
-
-var mainConfig = Object.assign({}, config, {
+module.exports = {
 	...defaultConfig,
 	...{
 		entry: {
 			index: path.resolve(process.cwd(), 'src', 'index.js'),
-			"theme": path.resolve(process.cwd(), 'src', 'theme.js'),
-		},
-		output: {
-			path: path.resolve(process.cwd(), 'build'),
-
+			"theme": path.resolve(process.cwd(), 'src', 'theme.js')
 		}
-	},
-});
-
-var pluginConfig = Object.assign({}, config, {
-	...defaultConfig,
-	...{
-		name: "plugin",
-		entry: path.resolve(__dirname, 'src/plugin-content/plugin.scss'),
-		output: {
-			path: path.resolve(__dirname, 'build/plugin-content'),
-			filename: "plugin-content.js",
-
-		},
 	}
-});
-
-// Return Array of Configurations
-module.exports = [
-	mainConfig,
-];
-
+}
