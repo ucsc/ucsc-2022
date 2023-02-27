349a4e062e142e6313a5e67cc14581e6b5788442
Rob Knight <raknight@ucsc.edu>
2023-02-27 12:13:20 -0800

    feat(Patterns): ✨ Add content with sidebar navigation pattern

---
+++ patterns/layout-66-33-with-navigation.php
@@ -0,0 +1,43 @@
+<?php
+ /**
+  * Title: Content with sidebar navigation
+  * Slug: ucsc-2022/layout-66-33-with-navigation
+  * Categories: layout, columns
+  */
+?>
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%">
+</div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%">
+<!-- wp:heading {"level":3,"fontSize":"base"} -->
+<h3 class="has-base-font-size"><strong>In this section</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:list {"className":"is-style-ucsc-side-navigation"} -->
+<ul class="is-style-ucsc-side-navigation"><!-- wp:list-item -->
+<li><a href="#">Add links for this section</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#">To this list</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#">Save the navigation list</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#">As a reusable block</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="#">To include it on additional pages</a></li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
