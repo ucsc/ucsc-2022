151b9b1feb22b7c819b6a573ee12caf101e59385
MlKilderkin <mlkilderkin@gmail.com>
2023-04-11 15:36:44 +0300

    UCSC-66 Add settings page to control theme behaviour

--- lib/acf.php
+++ lib/acf.php
@@ -49,3 +49,76 @@ if ( function_exists( 'acf_add_local_field_group' ) ) :
 	);
 
 endif;
+
+if ( function_exists( 'acf_add_options_page' ) ) {
+	acf_add_options_page( [
+		'page_title' => esc_html__( 'Theme options', 'ucsc-2022' ),
+		'menu_slug'  => esc_html__( 'ucsc-theme-options', 'ucsc-2022' ),
+	] );
+}
+
+if( function_exists('acf_add_local_field_group') ) {
+
+	acf_add_local_field_group( [
+		'key' 					=> 'ucsc_theme_options_group',
+		'title' 				=> 'Theme options',
+		'fields' 				=> [
+			[
+				'key' 				=> 'ucsc_theme_options_group_tab',
+				'label' 			=> esc_html__( 'General Options', 'ucsc-2022' ),
+				'name' 				=> '',
+				'aria-label' 		=> '',
+				'type' 				=> 'tab',
+				'instructions' 		=> '',
+				'required' 			=> 0,
+				'conditional_logic' => 0,
+				'wrapper' 			=> [
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				],
+				'placement' 		=> 'top',
+				'endpoint' 			=> 0,
+			],
+			[
+				'key' 				=> 'ucsc_theme_options_group_universal_logo_visibility',
+				'label' 			=> esc_html__( 'Universal Logo', 'ucsc-2022' ),
+				'name' 				=> 'toggle_universal_logo_visibility',
+				'aria-label' 		=> '',
+				'type' 				=> 'true_false',
+				'instructions' 		=> '',
+				'required' 			=> 0,
+				'conditional_logic' => 0,
+				'wrapper' 			=> [
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				],
+				'message' 			=> esc_html__( 'Display', 'ucsc-2022' ),
+				'default_value' 	=> 1,
+				'ui_on_text' 		=> '',
+				'ui_off_text' 		=> '',
+				'ui' 				=> 0,
+			],
+		],
+		'location' 				=> [
+			[
+				[
+					'param'    => 'options_page',
+					'operator' => '==',
+					'value'    => 'ucsc-theme-options',
+				],
+			],
+		],
+		'menu_order' 			=> 0,
+		'position' 				=> 'normal',
+		'style' 				=> 'default',
+		'label_placement' 		=> 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' 		=> '',
+		'active' 				=> true,
+		'description' 			=> '',
+		'show_in_rest' 			=> 0,
+	] );
+
+}
