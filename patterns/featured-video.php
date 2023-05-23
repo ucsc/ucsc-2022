9e26230ae40eab52a296c267f2e5fcd4474199ea
Rob Knight <raknight@ucsc.edu>
2023-05-23 09:37:06 -0700

    feat: 🎨 Add featured image and video patterns (#213)
    New patterns appear in the block editor and are generated on the page nicely.

---
+++ patterns/featured-video.php
@@ -0,0 +1,30 @@
+<?php
+ /**
+  * Title: Featured video
+  * Slug: ucsc-2022/featured-video
+  * Categories: Video
+  */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|40"}}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:embed {"url":"https://www.youtube.com/watch?v=dQw4w9WgXcQ","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+<figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+https://www.youtube.com/watch?v=dQw4w9WgXcQ
+</div><figcaption class="wp-element-caption">"Never Gonna Give You Up" by Rick Astley</figcaption></figure>
+<!-- /wp:embed -->
+
+<!-- wp:group {"layout":{"type":"constrained","wideSize":"60%"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textColor":"ucsc-primary-blue"} -->
+<h2 class="wp-block-heading has-ucsc-primary-blue-color has-text-color">Block heading</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Block text. <strong>Be sure to change the video</strong>. tempus mauris fermentum mi mattis vehicula enim efficitur, fusce justo curae natoque torquent lectus phasellus volutpat ad semper in. Dui lacinia duis parturient viverra tempus primis consectetur nostra morbi mattis nam ipsum, fringilla penatibus sociosqu nulla maecenas tincidunt phasellus risus praesent elit.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"ucsc-primary-blue","textColor":"white","className":"is-style-default"} -->
+<div class="wp-block-button is-style-default"><a class="wp-block-button__link has-white-color has-ucsc-primary-blue-background-color has-text-color has-background wp-element-button" href="https://youtube.com/ucsantacruz">Block call to action</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
