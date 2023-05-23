ed9cdad7db05e4ea3ee9ade7c219011a85be2394
Vinny Listrani <vinny@tri.be>
2023-05-23 14:20:45 -0500

    Update homepage hero to use default empty values.

--- patterns/homepage-hero.php
+++ patterns/homepage-hero.php
@@ -8,8 +8,8 @@
 ?>
 
 <!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"0"}},"className":"ucsc__homepage-hero","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull ucsc__homepage-hero"><!-- wp:cover {"url":"http://ucsc.local/wp-content/uploads/2013/12/2014-slider-mobile-behavior.mov","id":1690,"dimRatio":50,"backgroundType":"video","contentPosition":"center center","isDark":false,"align":"full","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|60","top":"var:preset|spacing|50"}}}} -->
-	<div class="wp-block-cover alignfull is-light" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--60)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><video class="wp-block-cover__video-background intrinsic-ignore" autoplay muted loop playsinline src="http://ucsc.local/wp-content/uploads/2013/12/2014-slider-mobile-behavior.mov" data-object-fit="cover"></video><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40","padding":{"top":"0","right":"0","bottom":"var:preset|spacing|80","left":"0"}}},"className":"ucsc__homepage-hero-header","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull ucsc__homepage-hero"><!-- wp:cover {"dimRatio":50,"contentPosition":"center center","isDark":false,"align":"full","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|60","top":"var:preset|spacing|50"}}}} -->
+	<div class="wp-block-cover alignfull is-light" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--60)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40","padding":{"top":"0","right":"0","bottom":"var:preset|spacing|80","left":"0"}}},"className":"ucsc__homepage-hero-header","layout":{"type":"default"}} -->
 			<div class="wp-block-group ucsc__homepage-hero-header" style="padding-top:0;padding-right:0;padding-bottom:var(--wp--preset--spacing--80);padding-left:0"><!-- wp:heading {"textAlign":"left","level":1,"style":{"spacing":{"padding":{"top":"8px","bottom":"8px","left":"var:preset|spacing|20","right":"var:preset|spacing|40"},"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"backgroundColor":"ucsc-primary-yellow","textColor":"ucsc-primary-blue","fontSize":"eight"} -->
 				<h1 class="wp-block-heading has-text-align-left has-ucsc-primary-blue-color has-ucsc-primary-yellow-background-color has-text-color has-background has-eight-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:8px;padding-right:var(--wp--preset--spacing--40);padding-bottom:8px;padding-left:var(--wp--preset--spacing--20)">Hero Title 1</h1>
 				<!-- /wp:heading -->
@@ -19,55 +19,55 @@
 				<!-- /wp:heading -->
 
 				<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left","flexWrap":"wrap","orientation":"horizontal"},"style":{"spacing":{"blockGap":"var:preset|spacing|30","margin":{"top":"var:preset|spacing|50"}}}} -->
-				<div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--50)"><!-- wp:button {"className":"is-style-ucsc-ocean"} -->
-					<div class="wp-block-button is-style-ucsc-ocean"><a class="wp-block-button__link wp-element-button">Call to Action</a></div>
+				<div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--50)"><!-- wp:button {"className":"is-style-ucsc-white"} -->
+					<div class="wp-block-button is-style-ucsc-white"><a class="wp-block-button__link wp-element-button">Call to Action</a></div>
 					<!-- /wp:button --></div>
 				<!-- /wp:buttons --></div>
 			<!-- /wp:group --></div></div>
 	<!-- /wp:cover -->
 
-	<!-- wp:cover {"url":"http://ucsc.local/wp-content/uploads/2008/06/michelle_049.jpg","id":766,"dimRatio":70,"overlayColor":"ucsc-primary-blue","align":"full","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|70","top":"0","right":"0","left":"0"}}}} -->
-	<div class="wp-block-cover alignfull" style="padding-top:0;padding-right:0;padding-bottom:var(--wp--preset--spacing--70);padding-left:0"><span aria-hidden="true" class="wp-block-cover__background has-ucsc-primary-blue-background-color has-background-dim-70 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-766" alt="Big Sur" src="http://ucsc.local/wp-content/uploads/2008/06/michelle_049.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50","padding":{"right":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"className":"ucsc__homepage-hero\u002d\u002dbottom","layout":{"type":"constrained","justifyContent":"center","contentSize":"1140px"}} -->
+	<!-- wp:cover {"dimRatio":70,"overlayColor":"ucsc-primary-blue","align":"full","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|70","top":"0","right":"0","left":"0"}}}} -->
+	<div class="wp-block-cover alignfull" style="padding-top:0;padding-right:0;padding-bottom:var(--wp--preset--spacing--70);padding-left:0"><span aria-hidden="true" class="wp-block-cover__background has-ucsc-primary-blue-background-color has-background-dim-70 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50","padding":{"right":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"className":"ucsc__homepage-hero\u002d\u002dbottom","layout":{"type":"constrained","justifyContent":"center","contentSize":"1140px"}} -->
 			<div class="wp-block-group ucsc__homepage-hero--bottom" style="padding-right:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|40"},"blockGap":{"top":"60px","left":"60px"}}},"backgroundColor":"white","textColor":"ucsc-primary-blue","className":"ucsc__homepage-hero-cards"} -->
 				<div class="wp-block-columns is-not-stacked-on-mobile ucsc__homepage-hero-cards has-ucsc-primary-blue-color has-white-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:column {"width":"31%","style":{"border":{"width":"4px"},"spacing":{"padding":{"right":"var:preset|spacing|20","bottom":"var:preset|spacing|30","left":"var:preset|spacing|20","top":"var:preset|spacing|40"}}},"borderColor":"ucsc-primary-yellow","className":"ucsc__homepage-hero-card"} -->
+					<div class="wp-block-column ucsc__homepage-hero-card has-border-color has-ucsc-primary-yellow-border-color" style="border-width:4px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--20);flex-basis:31%"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":"ucsc__homepage-hero-card-title","fontSize":"eight"} -->
+						<p class="has-text-align-center ucsc__homepage-hero-card-title has-eight-font-size" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;font-style:normal;font-weight:700"><mark style="background-color:#ffffff" class="has-inline-color has-ucsc-primary-blue-color">#1</mark></p>
+						<!-- /wp:paragraph -->
+
+						<!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":1.6,"fontStyle":"normal","fontWeight":"500"}},"fontSize":"three"} -->
+						<p class="has-text-align-center has-three-font-size" style="font-style:normal;font-weight:500;line-height:1.6">Content area for column description</p>
+						<!-- /wp:paragraph -->
+
+						<!-- wp:paragraph {"align":"center","textColor":"ucsc-secondary-blue"} -->
+						<p class="has-text-align-center has-ucsc-secondary-blue-color has-text-color">byline for column</p>
+						<!-- /wp:paragraph --></div>
+					<!-- /wp:column -->
+
+					<!-- wp:column {"width":"31%","style":{"border":{"width":"4px"},"spacing":{"padding":{"right":"var:preset|spacing|20","bottom":"var:preset|spacing|30","left":"var:preset|spacing|20","top":"var:preset|spacing|40"}}},"borderColor":"ucsc-primary-yellow","className":"ucsc__homepage-hero-card"} -->
 					<div class="wp-block-column ucsc__homepage-hero-card has-border-color has-ucsc-primary-yellow-border-color" style="border-width:4px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--20);flex-basis:31%"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":"ucsc__homepage-hero-card-title","fontSize":"eight"} -->
 						<p class="has-text-align-center ucsc__homepage-hero-card-title has-eight-font-size" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;font-style:normal;font-weight:700"><mark style="background-color:#ffffff" class="has-inline-color has-ucsc-primary-blue-color">#2</mark></p>
 						<!-- /wp:paragraph -->
 
 						<!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":1.6,"fontStyle":"normal","fontWeight":"500"}},"fontSize":"three"} -->
-						<p class="has-text-align-center has-three-font-size" style="font-style:normal;font-weight:500;line-height:1.6">For making an impact on the world</p>
+						<p class="has-text-align-center has-three-font-size" style="font-style:normal;font-weight:500;line-height:1.6">Content area for column description</p>
 						<!-- /wp:paragraph -->
 
 						<!-- wp:paragraph {"align":"center","textColor":"ucsc-secondary-blue"} -->
-						<p class="has-text-align-center has-ucsc-secondary-blue-color has-text-color">by Princeton Review</p>
+						<p class="has-text-align-center has-ucsc-secondary-blue-color has-text-color">byline for column</p>
 						<!-- /wp:paragraph --></div>
 					<!-- /wp:column -->
 
 					<!-- wp:column {"width":"31%","style":{"border":{"width":"4px"},"spacing":{"padding":{"right":"var:preset|spacing|20","bottom":"var:preset|spacing|30","left":"var:preset|spacing|20","top":"var:preset|spacing|40"}}},"borderColor":"ucsc-primary-yellow","className":"ucsc__homepage-hero-card"} -->
 					<div class="wp-block-column ucsc__homepage-hero-card has-border-color has-ucsc-primary-yellow-border-color" style="border-width:4px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--20);flex-basis:31%"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":"ucsc__homepage-hero-card-title","fontSize":"eight"} -->
-						<p class="has-text-align-center ucsc__homepage-hero-card-title has-eight-font-size" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;font-style:normal;font-weight:700"><mark style="background-color:#ffffff" class="has-inline-color has-ucsc-primary-blue-color">Top 10</mark></p>
+						<p class="has-text-align-center ucsc__homepage-hero-card-title has-eight-font-size" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;font-style:normal;font-weight:700"><mark style="background-color:#ffffff" class="has-inline-color has-ucsc-primary-blue-color">#3</mark></p>
 						<!-- /wp:paragraph -->
 
 						<!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":1.6,"fontStyle":"normal","fontWeight":"500"}},"fontSize":"three"} -->
-						<p class="has-text-align-center has-three-font-size" style="font-style:normal;font-weight:500;line-height:1.6">For making an impact on the world</p>
+						<p class="has-text-align-center has-three-font-size" style="font-style:normal;font-weight:500;line-height:1.6">Content area for column description</p>
 						<!-- /wp:paragraph -->
 
 						<!-- wp:paragraph {"align":"center","textColor":"ucsc-secondary-blue"} -->
-						<p class="has-text-align-center has-ucsc-secondary-blue-color has-text-color">by Princeton Review</p>
-						<!-- /wp:paragraph --></div>
-					<!-- /wp:column -->
-
-					<!-- wp:column {"width":"31%","style":{"border":{"width":"4px"},"spacing":{"padding":{"right":"var:preset|spacing|20","bottom":"var:preset|spacing|30","left":"var:preset|spacing|20","top":"var:preset|spacing|40"}}},"borderColor":"ucsc-primary-yellow","className":"ucsc__homepage-hero-card"} -->
-					<div class="wp-block-column ucsc__homepage-hero-card has-border-color has-ucsc-primary-yellow-border-color" style="border-width:4px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--20);flex-basis:31%"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":"ucsc__homepage-hero-card-title","fontSize":"eight"} -->
-						<p class="has-text-align-center ucsc__homepage-hero-card-title has-eight-font-size" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;font-style:normal;font-weight:700"><mark style="background-color:#ffffff" class="has-inline-color has-ucsc-primary-blue-color">Long</mark></p>
-						<!-- /wp:paragraph -->
-
-						<!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":1.6,"fontStyle":"normal","fontWeight":"500"}},"fontSize":"three"} -->
-						<p class="has-text-align-center has-three-font-size" style="font-style:normal;font-weight:500;line-height:1.6">For making an impact on the world</p>
-						<!-- /wp:paragraph -->
-
-						<!-- wp:paragraph {"align":"center","textColor":"ucsc-secondary-blue"} -->
-						<p class="has-text-align-center has-ucsc-secondary-blue-color has-text-color">by Princeton Review</p>
+						<p class="has-text-align-center has-ucsc-secondary-blue-color has-text-color">byline for column</p>
 						<!-- /wp:paragraph --></div>
 					<!-- /wp:column --></div>
 				<!-- /wp:columns -->
