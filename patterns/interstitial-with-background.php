2fd45780f55008a92f22c0d8fcd479fdd6eb8f22
Brett Cooper <brett@thewiznerd.com>
2023-05-29 10:33:15 -0600

    Added interstial block patterns.

---
+++ patterns/interstitial-with-background.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: Interstitial with background
+ * Slug: ucsc-2022/interstitial-with-background
+ * Block Types: core/paragraph, core/list
+ * Categories: ucsc, buttons, call-to-action, headers, text
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"ucsc-green","textColor":"black","className":"ucsc__interstitial","layout":{"type":"constrained"}} -->
+<div class="wp-block-group ucsc__interstitial has-black-color has-ucsc-green-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:group {"lock":{"move":true,"remove":true},"style":{"spacing":{"blockGap":"var:preset|spacing|20","padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"wrap","justifyContent":"center"}} -->
+<div class="wp-block-group" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:heading {"textAlign":"left","style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"fontSize":"four"} -->
+<h2 class="wp-block-heading has-text-align-left has-four-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">Interstitial Heading</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"lock":{"move":true,"remove":true},"className":"ucsc__interstitial-content","layout":{"type":"constrained"}} -->
+<div class="wp-block-group ucsc__interstitial-content"><!-- wp:paragraph -->
+<p>Add your interstitial body content here.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-ucsc-blue"} -->
+<div class="wp-block-button is-style-ucsc-blue"><a class="wp-block-button__link wp-element-button">Button Text</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
