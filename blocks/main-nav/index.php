fca2d1ae98baca07a952fbfe7a6953de54ad9153
Brett Cooper <brett@thewiznerd.com>
2023-06-08 12:36:39 -0600

    Undoing bad, bad merge.

--- blocks/main-nav/index.php
+++ blocks/main-nav/index.php
@@ -1,5 +1,14 @@
-<div>
-	<?php echo wp_nav_menu( [
-		'theme_location' => 'primary'
-	] );?>
-</div>
+<button class="site-header__navigation-toggle">
+	<span></span>
+	<span></span>
+	<span></span>
+</button>
+
+<nav class="site-header__navigation alignfull" data-js="main-nav">
+	<?php echo wp_nav_menu(
+		array(
+			'theme_location' => 'primary',
+			'depth'          => 2,
+		)
+	);?>
+</nav>
