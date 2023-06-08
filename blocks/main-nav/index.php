3324cf62dcf8d539529eec086e52f5c7d5a1b771
Brett Cooper <brett@thewiznerd.com>
2023-06-08 08:39:01 -0600

    Pulled back main nav merge.

--- blocks/main-nav/index.php
+++ blocks/main-nav/index.php
@@ -1,14 +1,5 @@
-<button class="site-header__navigation-toggle">
-	<span></span>
-	<span></span>
-	<span></span>
-</button>
-
-<nav class="site-header__navigation alignfull" data-js="main-nav">
-	<?php echo wp_nav_menu(
-		array(
-			'theme_location' => 'primary',
-			'depth'          => 2,
-		)
-	);?>
-</nav>
+<div>
+	<?php echo wp_nav_menu( [
+		'theme_location' => 'primary'
+	] );?>
+</div>
