109771faac8725615a8a084caa320b6455bbac41
Vinny Listrani <vinny@tri.be>
2023-06-12 09:43:57 -0500

    Added 3 card patterns for use with other patterns.

---
+++ patterns/card-plain-cta.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Title: Card w/ CTA
+ * Slug: ucsc-2022/card-plain-cta
+ * Categories: ucsc, cards
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"className":"ucsc__card ucsc__card-plain-cta","layout":{"type":"default"}} -->
+<div class="wp-block-group ucsc__card ucsc__card-plain"><!-- wp:image {"id":770,"sizeSlug":"sixteen-nine","linkDestination":"none"} -->
+	<figure class="wp-block-image size-sixteen-nine"><img src="https://ucsc.local/wp-content/uploads/2008/06/img_0767-800x496.jpg" alt="Huatulco Coastline" class="wp-image-770"/></figure>
+	<!-- /wp:image -->
+
+	<!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+	<h3 class="wp-block-heading" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">Card Title</h3>
+	<!-- /wp:heading -->
+
+	<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"fontSize":"base"} -->
+	<p class="has-base-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel ante nec diam placerat vulputate. Fusce ut dui sagittis lacus rutrum tristique ut vitae leo. Phasellus ultrices nunc nec tempus condimentum. Sed facilisis scelerisque lobortis. Suspendisse potenti. Duis iaculis at mi eget porttitor. Donec elementum lacus ut fermentum aliquet.</p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:buttons {"className":"ucsc__card-cta","style":{"spacing":{"blockGap":"0"}}} -->
+	<div class="wp-block-buttons ucsc__card-cta"><!-- wp:button {"className":"is-style-ucsc-blue"} -->
+		<div class="wp-block-button is-style-ucsc-blue"><a class="wp-block-button__link wp-element-button">Call To Action</a></div>
+		<!-- /wp:button --></div>
+	<!-- /wp:buttons --></div>
+<!-- /wp:group -->
---
+++ patterns/card-yellow.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Title: Card: Yellow w/ CTA
+ * Slug: ucsc-2022/card-yellow-cta
+ * Categories: ucsc, cards
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"backgroundColor":"ucsc-primary-yellow","className":"ucsc__card ucsc__card-yellow","layout":{"type":"default"}} -->
+<div class="wp-block-group ucsc__card ucsc__card-yellow has-ucsc-primary-yellow-background-color has-background" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"id":770,"sizeSlug":"sixteen-nine","linkDestination":"none"} -->
+	<figure class="wp-block-image size-sixteen-nine"><img src="https://ucsc.local/wp-content/uploads/2008/06/img_0767-800x496.jpg" alt="Huatulco Coastline" class="wp-image-770"/></figure>
+	<!-- /wp:image -->
+
+	<!-- wp:group {"style":{"spacing":{"padding":{"top":"40px","right":"var:preset|spacing|20","bottom":"var:preset|spacing|40","left":"var:preset|spacing|20"},"blockGap":"var:preset|spacing|20"}},"className":"ucsc__card-inner","layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+	<div class="wp-block-group ucsc__card-inner" style="padding-top:40px;padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--20)"><!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+		<h3 class="wp-block-heading" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">Card Title</h3>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph {"fontSize":"base"} -->
+		<p class="has-base-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel ante nec diam placerat vulputate. Fusce ut dui sagittis lacus rutrum tristique ut vitae leo. </p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:buttons {"className":"ucsc__card-cta"} -->
+		<div class="wp-block-buttons ucsc__card-cta"><!-- wp:button {"className":"is-style-ucsc-blue"} -->
+			<div class="wp-block-button is-style-ucsc-blue"><a class="wp-block-button__link wp-element-button">Call To Action</a></div>
+			<!-- /wp:button --></div>
+		<!-- /wp:buttons --></div>
+	<!-- /wp:group --></div>
+<!-- /wp:group -->
