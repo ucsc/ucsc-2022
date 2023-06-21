46925f96b3652ab59a99e09d9938d2e69f2a24d1
Brett Cooper <brett@thewiznerd.com>
2023-06-21 09:11:08 -0600

    Removed spacing from styles.

--- patterns/interstitial.php
+++ patterns/interstitial.php
@@ -6,8 +6,8 @@
  * Categories: ucsc, buttons, call-to-action, headers, text
  */
 ?>
-<!-- wp:group {"lock":{"move":false,"remove":false},"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"backgroundColor":"white","textColor":"black","className":"ucsc__interstitial","layout":{"type":"constrained","contentSize":"1280px","justifyContent":"center"},"fontSize":"two"} -->
-<div class="wp-block-group alignfull ucsc__interstitial has-black-color has-white-background-color has-text-color has-background has-two-font-size" style="padding-right:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"textColor":"ucsc-primary-blue","fontSize":"four"} -->
+<!-- wp:group {"lock":{"move":false,"remove":false},"align":"full","backgroundColor":"white","textColor":"black","className":"ucsc__interstitial","layout":{"type":"constrained","contentSize":"1280px","justifyContent":"center"},"fontSize":"two"} -->
+<div class="wp-block-group alignfull ucsc__interstitial has-black-color has-white-background-color has-text-color has-background has-two-font-size"><!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"textColor":"ucsc-primary-blue","fontSize":"four"} -->
 <h2 class="wp-block-heading has-text-align-center has-ucsc-primary-blue-color has-text-color has-four-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">Interstitial Heading</h2>
 <!-- /wp:heading -->
 
