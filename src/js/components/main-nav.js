07b4540cce00be881ebddd611d587ec227fa9f04
Brett Cooper <brett@thewiznerd.com>
2023-07-12 11:06:59 -0600

    Fixed linting.

--- src/js/components/main-nav.js
+++ src/js/components/main-nav.js
@@ -27,16 +27,16 @@ const showMenu = () => {
 	// Stop observing the sticky header when we open the menu, because it's going
 	// to float to the top of the viewport and not reflect our actual scroll
 	// position.
-	cache.stickyHeaderObserver.unobserve(cache.stickyHeader);
+	cache.stickyHeaderObserver.unobserve( cache.stickyHeader );
 	bodyLock.lock();
 
-	document.documentElement.classList.add('main-nav--is-open');
-	if (!cache.navIsOpen) {
-		document.documentElement.classList.add('main-nav--is-opening');
+	document.documentElement.classList.add( 'main-nav--is-open' );
+	if ( ! cache.navIsOpen ) {
+		document.documentElement.classList.add( 'main-nav--is-opening' );
 	}
 
-	cache.nav.setAttribute('aria-hidden', false);
-	cache.navToggle.setAttribute('aria-label', 'Hide main navigation menu');
+	cache.nav.setAttribute( 'aria-hidden', false );
+	cache.navToggle.setAttribute( 'aria-label', 'Hide main navigation menu' );
 	cache.navIsOpen = true;
 };
 
@@ -44,28 +44,28 @@ const showMenu = () => {
  * Hide the main-nav menu on mobile.
  */
 const hideMenu = () => {
-	document.documentElement.classList.remove('main-nav--is-open');
-	if (cache.navIsOpen) {
-		document.documentElement.classList.add('main-nav--is-closing');
+	document.documentElement.classList.remove( 'main-nav--is-open' );
+	if ( cache.navIsOpen ) {
+		document.documentElement.classList.add( 'main-nav--is-closing' );
 	}
 
-	cache.nav.setAttribute('aria-hidden', true);
-	cache.navToggle.setAttribute('aria-label', 'Show main navigation menu');
+	cache.nav.setAttribute( 'aria-hidden', true );
+	cache.navToggle.setAttribute( 'aria-label', 'Show main navigation menu' );
 
 	// Reset submenus when the main nav is hidden.
-	cache.menuItems.forEach((data) => {
-		deactivateMenuItem(data);
-	});
+	cache.menuItems.forEach( ( data ) => {
+		deactivateMenuItem( data );
+	} );
 
 	// If we unlock the body before our closing animation completes, the scroll
 	// position will be off in some cases. For that reason, we unlock the body in
 	// our transitionended event handler. However, if the nav is already hidden,
 	// no transition will occur, so we handle that here.
-	if (!cache.navIsOpen && bodyLock.isLocked()) {
+	if ( ! cache.navIsOpen && bodyLock.isLocked() ) {
 		bodyLock.unlock();
 	}
 
-	cache.stickyHeaderObserver.observe(cache.stickyHeader);
+	cache.stickyHeaderObserver.observe( cache.stickyHeader );
 	cache.navIsOpen = false;
 };
 
@@ -73,7 +73,7 @@ const hideMenu = () => {
  * Toggle the main-nav menu on mobile.
  */
 const toggleMenu = () => {
-	if (cache.navIsOpen) {
+	if ( cache.navIsOpen ) {
 		hideMenu();
 	} else {
 		showMenu();
@@ -84,18 +84,18 @@ const toggleMenu = () => {
  * Add JS behaviors to the main-nav menu.
  */
 const initMainNav = () => {
-	cache.nav = document.querySelector('[data-js="main-nav"]');
-	if (!cache.nav) {
+	cache.nav = document.querySelector( '[data-js="main-nav"]' );
+	if ( ! cache.nav ) {
 		return;
 	}
 
 	// Observer when our sticky header becomes "stuck".
-	cache.stickyHeader = document.querySelector('.header-region');
+	cache.stickyHeader = document.querySelector( '.header-region' );
 	cache.stickyHeaderObserver = new window.IntersectionObserver(
-		(entries) => {
+		( entries ) => {
 			document.documentElement.classList.toggle(
 				'header-region--stuck',
-				entries[0].intersectionRatio < 1
+				entries[ 0 ].intersectionRatio < 1
 			);
 		},
 		{
@@ -106,78 +106,91 @@ const initMainNav = () => {
 
 	// Get our mobile-to-desktop breakpoint.
 	const breakpoint = window
-		.getComputedStyle(cache.nav)
-		.getPropertyValue('--desktop-breakpoint')
-		.replace('px', '');
-	if (breakpoint) {
-		cache.desktopBreakpoint = parseInt(breakpoint);
+		.getComputedStyle( cache.nav )
+		.getPropertyValue( '--desktop-breakpoint' )
+		.replace( 'px', '' );
+	if ( breakpoint ) {
+		cache.desktopBreakpoint = parseInt( breakpoint );
 	}
 
 	// Update CSS classes for navigation animation states.
-	const navigation = document.querySelector('.site-header__navigation');
-	navigation.addEventListener('transitionend', () => {
+	const navigation = document.querySelector( '.site-header__navigation' );
+	navigation.addEventListener( 'transitionend', () => {
 		document.documentElement.classList.remove(
 			'main-nav--is-opening',
 			'main-nav--is-closing'
 		);
 		// Unlock the body after the nav is closed.
-		if (!cache.navIsOpen && bodyLock.isLocked()) {
+		if ( ! cache.navIsOpen && bodyLock.isLocked() ) {
 			bodyLock.unlock();
 		}
-	});
+	} );
 
 	// Add mobile nav toggle.
-	cache.navToggle = document.querySelector('.site-header__navigation-toggle');
-	cache.navToggle.addEventListener('click', toggleMenu);
+	cache.navToggle = document.querySelector(
+		'.site-header__navigation-toggle'
+	);
+	cache.navToggle.addEventListener( 'click', toggleMenu );
 
 	// Add menu item behaviors.
-	cache.nav.querySelectorAll('.menu > .menu-item').forEach((menuItem) => {
-		// Cache menu item data, including references to the different elements
-		// connected to this menu item.
-		const data = {
-			menuItem,
-			submenu: null,
-			toggle: null,
-			links: null,
-			isActive: false,
-		};
-		cache.menuItems.push(data);
+	cache.nav
+		.querySelectorAll( '.menu > .menu-item' )
+		.forEach( ( menuItem ) => {
+			// Cache menu item data, including references to the different elements
+			// connected to this menu item.
+			const data = {
+				menuItem,
+				submenu: null,
+				toggle: null,
+				links: null,
+				isActive: false,
+			};
+			cache.menuItems.push( data );
 
-		// Listen for mouse and keyboard interactions.
-		data.menuItem.addEventListener('focusin', () => updateMenuItem(data));
-		data.menuItem.addEventListener('focusout', () => updateMenuItem(data));
-		data.menuItem.addEventListener('mouseover', () => updateMenuItem(data));
-		data.menuItem.addEventListener('mouseout', () => updateMenuItem(data));
+			// Listen for mouse and keyboard interactions.
+			data.menuItem.addEventListener( 'focusin', () =>
+				updateMenuItem( data )
+			);
+			data.menuItem.addEventListener( 'focusout', () =>
+				updateMenuItem( data )
+			);
+			data.menuItem.addEventListener( 'mouseover', () =>
+				updateMenuItem( data )
+			);
+			data.menuItem.addEventListener( 'mouseout', () =>
+				updateMenuItem( data )
+			);
 
-		// There's nothing more to do if this menu item has no submenu.
-		const submenuEl = data.menuItem.querySelector(':scope > .sub-menu');
-		if (!submenuEl) {
-			return;
-		}
-
-		// Cache submenu links.
-		data.links = submenuEl.querySelectorAll('a');
-
-		// Add a wrapper around the submenu for animating height transitions.
-		data.submenu = document.createElement('div');
-		data.submenu.classList.add('sub-menu-wrapper');
-		data.submenu.append(submenuEl);
-
-		// Add a submenu toggle button.
-		data.toggle = document.createElement('button');
-		data.toggle.classList.add('sub-menu-toggle');
-		data.toggle.addEventListener('click', () => {
-			if (data.isActive) {
-				deactivateMenuItem(data);
-			} else {
-				activateMenuItem(data);
+			// There's nothing more to do if this menu item has no submenu.
+			const submenuEl =
+				data.menuItem.querySelector( ':scope > .sub-menu' );
+			if ( ! submenuEl ) {
+				return;
 			}
-		});
 
-		// Insert the toggle before the submenu.
-		data.menuItem.append(data.toggle);
-		data.menuItem.append(data.submenu);
-	});
+			// Cache submenu links.
+			data.links = submenuEl.querySelectorAll( 'a' );
+
+			// Add a wrapper around the submenu for animating height transitions.
+			data.submenu = document.createElement( 'div' );
+			data.submenu.classList.add( 'sub-menu-wrapper' );
+			data.submenu.append( submenuEl );
+
+			// Add a submenu toggle button.
+			data.toggle = document.createElement( 'button' );
+			data.toggle.classList.add( 'sub-menu-toggle' );
+			data.toggle.addEventListener( 'click', () => {
+				if ( data.isActive ) {
+					deactivateMenuItem( data );
+				} else {
+					activateMenuItem( data );
+				}
+			} );
+
+			// Insert the toggle before the submenu.
+			data.menuItem.append( data.toggle );
+			data.menuItem.append( data.submenu );
+		} );
 
 	// Start closed.
 	hideMenu();
@@ -200,21 +213,21 @@ const menuIsMobile = () => {
  *
  * @param {Object} data The menu item data object from cache.menuItems.
  */
-const updateMenuItem = (data) => {
+const updateMenuItem = ( data ) => {
 	// On mobile, only open menu items when the associated toggle button is
 	// pressed.
-	if (menuIsMobile()) {
+	if ( menuIsMobile() ) {
 		return;
 	}
 
 	// On desktop, open menu items on :focus-within or :hover.
 	const shouldBeActive =
-		data.menuItem.matches(':focus-within') ||
-		data.menuItem.matches(':hover');
-	if (data.isActive && !shouldBeActive) {
-		deactivateMenuItem(data);
-	} else if (!data.isActive && shouldBeActive) {
-		activateMenuItem(data);
+		data.menuItem.matches( ':focus-within' ) ||
+		data.menuItem.matches( ':hover' );
+	if ( data.isActive && ! shouldBeActive ) {
+		deactivateMenuItem( data );
+	} else if ( ! data.isActive && shouldBeActive ) {
+		activateMenuItem( data );
 	}
 };
 
@@ -223,14 +236,14 @@ const updateMenuItem = (data) => {
  *
  * @param {Object} data The menu item data object from cache.menuItems.
  */
-const activateMenuItem = (data) => {
-	data.menuItem.classList.add('menu-item--is-active');
-	if (data.submenu) {
-		data.submenu.setAttribute('aria-hidden', false);
-		data.toggle.setAttribute('aria-label', 'Hide submenu');
-		data.links.forEach((link) => {
-			link.removeAttribute('tabIndex');
-		});
+const activateMenuItem = ( data ) => {
+	data.menuItem.classList.add( 'menu-item--is-active' );
+	if ( data.submenu ) {
+		data.submenu.setAttribute( 'aria-hidden', false );
+		data.toggle.setAttribute( 'aria-label', 'Hide submenu' );
+		data.links.forEach( ( link ) => {
+			link.removeAttribute( 'tabIndex' );
+		} );
 	}
 	data.isActive = true;
 };
@@ -240,41 +253,41 @@ const activateMenuItem = (data) => {
  *
  * @param {Object} data The menu item data object from cache.menuItems.
  */
-const deactivateMenuItem = (data) => {
-	data.menuItem.classList.remove('menu-item--is-active');
-	if (data.submenu) {
-		data.submenu.setAttribute('aria-hidden', true);
-		data.toggle.setAttribute('aria-label', 'Show submenu');
-		data.links.forEach((link) => {
-			link.setAttribute('tabIndex', -1);
-		});
+const deactivateMenuItem = ( data ) => {
+	data.menuItem.classList.remove( 'menu-item--is-active' );
+	if ( data.submenu ) {
+		data.submenu.setAttribute( 'aria-hidden', true );
+		data.toggle.setAttribute( 'aria-label', 'Show submenu' );
+		data.links.forEach( ( link ) => {
+			link.setAttribute( 'tabIndex', -1 );
+		} );
 	}
 	data.isActive = false;
 };
 
 const handleResize = () => {
 	// Find the site header height.
-	const siteHeader = document.querySelector('.site-header__header');
+	const siteHeader = document.querySelector( '.site-header__header' );
 	document.documentElement.style.setProperty(
 		'--site-header-height',
 		siteHeader.clientHeight + 'px'
 	);
 
-	const navElement = document.querySelector('[data-js="main-nav"]');
-	if (state.v_width >= NAVIGATION_BREAKPOINT) {
-		navElement.setAttribute('aria-hidden', 'false');
+	const navElement = document.querySelector( '[data-js="main-nav"]' );
+	if ( state.v_width >= NAVIGATION_BREAKPOINT ) {
+		navElement.setAttribute( 'aria-hidden', 'false' );
 		return;
 	}
 
-	navElement.setAttribute('aria-hidden', 'true');
+	navElement.setAttribute( 'aria-hidden', 'true' );
 };
 
 const bindEvents = () => {
-	on(document, 'ucsc/resize_executed', handleResize);
+	on( document, 'ucsc/resize_executed', handleResize );
 };
 
 const init = () => {
-	if (!document.querySelector('[data-js="main-nav"]')) {
+	if ( ! document.querySelector( '[data-js="main-nav"]' ) ) {
 		return;
 	}
 
--- wp-blocks/styles.js
+++ wp-blocks/styles.js
@@ -1,90 +1,90 @@
 /**
  *  Block styles and variations and variations
  */
-wp.domReady(() => {
+wp.domReady( () => {
 	// Styles for core/group
-	wp.blocks.registerBlockStyle('core/paragraph', {
+	wp.blocks.registerBlockStyle( 'core/paragraph', {
 		name: 'ucsc-intro-text',
 		label: 'Intro text',
 		style_handle: 'ucsc-paragraph',
-	});
+	} );
 	// Styles for core/list
-	wp.blocks.registerBlockStyle('core/list', {
+	wp.blocks.registerBlockStyle( 'core/list', {
 		name: 'ucsc-side-navigation',
 		label: 'Side Navigation',
 		style_handle: 'ucsc-list',
-	});
-	wp.blocks.registerBlockStyle('core/list', {
+	} );
+	wp.blocks.registerBlockStyle( 'core/list', {
 		name: 'ucsc-upper-alpha-list',
 		label: 'Upper alpha',
 		style_handle: 'ucsc-list',
-	});
-	wp.blocks.registerBlockStyle('core/list', {
+	} );
+	wp.blocks.registerBlockStyle( 'core/list', {
 		name: 'ucsc-lower-alpha-list',
 		label: 'Lower alpha',
 		style_handle: 'ucsc-list',
-	});
+	} );
 	// Styles for core/button
-	wp.blocks.registerBlockStyle('core/button', {
+	wp.blocks.registerBlockStyle( 'core/button', {
 		name: 'ucsc-blue',
 		label: 'Blue',
 		style_handle: 'ucsc-button',
-	});
-	wp.blocks.registerBlockStyle('core/button', {
+	} );
+	wp.blocks.registerBlockStyle( 'core/button', {
 		name: 'ucsc-black',
 		label: 'Black',
 		style_handle: 'ucsc-button',
-	});
-	wp.blocks.registerBlockStyle('core/button', {
+	} );
+	wp.blocks.registerBlockStyle( 'core/button', {
 		name: 'ucsc-gold',
 		label: 'Gold',
 		style_handle: 'ucsc-button',
-	});
-	wp.blocks.registerBlockStyle('core/button', {
+	} );
+	wp.blocks.registerBlockStyle( 'core/button', {
 		name: 'ucsc-ocean',
 		label: 'Ocean',
 		style_handle: 'ucsc-button',
-	});
-	wp.blocks.registerBlockStyle('core/button', {
+	} );
+	wp.blocks.registerBlockStyle( 'core/button', {
 		name: 'ucsc-white',
 		label: 'White',
 		style_handle: 'ucsc-button',
-	});
-	wp.blocks.unregisterBlockStyle('core/button', 'outline');
-	wp.blocks.unregisterBlockStyle('core/button', 'fill');
+	} );
+	wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
+	wp.blocks.unregisterBlockStyle( 'core/button', 'fill' );
 
 	// Styles for core/separator
-	wp.blocks.unregisterBlockStyle('core/separator', 'default');
-	wp.blocks.unregisterBlockStyle('core/separator', 'dots');
-	wp.blocks.unregisterBlockStyle('core/separator', 'wide');
+	wp.blocks.unregisterBlockStyle( 'core/separator', 'default' );
+	wp.blocks.unregisterBlockStyle( 'core/separator', 'dots' );
+	wp.blocks.unregisterBlockStyle( 'core/separator', 'wide' );
 
 	// Styles for core/quote
-	wp.blocks.unregisterBlockStyle('core/quote', 'default');
-	wp.blocks.unregisterBlockStyle('core/quote', 'plain');
+	wp.blocks.unregisterBlockStyle( 'core/quote', 'default' );
+	wp.blocks.unregisterBlockStyle( 'core/quote', 'plain' );
 
 	// Styles for core/image
-	wp.blocks.unregisterBlockStyle('core/image', 'default');
-	wp.blocks.unregisterBlockStyle('core/image', 'rounded');
+	wp.blocks.unregisterBlockStyle( 'core/image', 'default' );
+	wp.blocks.unregisterBlockStyle( 'core/image', 'rounded' );
 
 	// Styles for core/rss
-	wp.blocks.registerBlockStyle('core/rss', {
+	wp.blocks.registerBlockStyle( 'core/rss', {
 		name: 'ucsc-rss-block',
 		label: 'Home Block',
 		style_handle: 'ucsc-rss',
-	});
-	wp.blocks.registerBlockStyle('core/rss', {
+	} );
+	wp.blocks.registerBlockStyle( 'core/rss', {
 		name: 'ucsc-rss-list',
 		label: 'Simple List',
 		isDefault: true,
 		style_handle: 'ucsc-rss',
-	});
+	} );
 	// Variation for core/spacer
-	wp.blocks.registerBlockVariation('core/spacer', {
+	wp.blocks.registerBlockVariation( 'core/spacer', {
 		isDefault: true,
 		name: 'spacer',
 		title: 'Spacer',
 		attributes: {
 			height: '4rem',
 		},
-	});
-});
+	} );
+} );
