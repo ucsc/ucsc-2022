294f0cdf64e5a76209457f0f2074858d1dbc2293
Rob Knight <raknight@ucsc.edu>
2023-08-02 17:21:46 -0700

    refactor: ✨ Remove old patterns and update categories of existing ones

---
+++ patterns/sidebar-with-navigation.php
@@ -0,0 +1,21 @@
+<?php
+ /**
+  * Title: Content with left navigation
+  * Slug: ucsc-2022/sidebar-with-navigation
+  * Categories: navigation
+  */
+?>
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:heading {"level":3,"fontSize":"base"} -->
+<h3 class="wp-block-heading has-base-font-size"><strong>Navigation header</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:navigation {"ref":2910,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
