df8354245fd3d536627a89d0ccd0f7380f401299
Brett Cooper <brett@thewiznerd.com>
2023-09-26 11:48:57 -0600

    Updated 3-column card grid block pattern.

--- patterns/card-grid-3-columns.php
+++ patterns/card-grid-3-columns.php
@@ -9,56 +9,68 @@
 
 <!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|50","right":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"className":"ucsc__3-col-card-grid","layout":{"type":"constrained","contentSize":"1280px"}} -->
 <div class="wp-block-group alignfull ucsc__3-col-card-grid" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--20)"><!-- wp:heading -->
-	<h2 class="wp-block-heading">3-Column Card Grid</h2>
-	<!-- /wp:heading -->
+<h2 class="wp-block-heading">3-Column Card Grid</h2>
+<!-- /wp:heading -->
 
-	<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50","left":"var:preset|spacing|20"}}},"className":"ucsc__card-grid"} -->
-	<div class="wp-block-columns ucsc__card-grid"><!-- wp:column -->
-		<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"className":"ucsc__card ucsc__card-plain","layout":{"type":"default"}} -->
-			<div class="wp-block-group ucsc__card ucsc__card-plain"><!-- wp:image -->
-				<figure class="wp-block-image"><img alt=""/></figure>
-				<!-- /wp:image -->
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50","left":"var:preset|spacing|20"}}},"className":"ucsc__card-grid"} -->
+<div class="wp-block-columns ucsc__card-grid"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"className":"ucsc__card ucsc__card-plain","layout":{"type":"default"}} -->
+<div class="wp-block-group ucsc__card ucsc__card-plain"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
 
-				<!-- wp:heading {"level":3} -->
-				<h3 class="wp-block-heading">Card Title</h3>
-				<!-- /wp:heading -->
+<!-- wp:paragraph {"className":"ucsc__card-overline","fontSize":"small"} -->
+<p class="ucsc__card-overline has-small-font-size">Overline</p>
+<!-- /wp:paragraph -->
 
-				<!-- wp:paragraph {"fontSize":"base"} -->
-				<p class="has-base-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel ante nec diam placerat vulputate. Fusce ut dui sagittis lacus rutrum tristique ut vitae leo. Phasellus ultrices nunc nec tempus condimentum. Sed facilisis scelerisque lobortis. Suspendisse potenti. Duis iaculis at mi eget porttitor. Donec elementum lacus ut fermentum aliquet.</p>
-				<!-- /wp:paragraph --></div>
-			<!-- /wp:group --></div>
-		<!-- /wp:column -->
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Card Title</h3>
+<!-- /wp:heading -->
 
-		<!-- wp:column -->
-		<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"className":"ucsc__card ucsc__card-plain","layout":{"type":"default"}} -->
-			<div class="wp-block-group ucsc__card ucsc__card-plain"><!-- wp:image -->
-				<figure class="wp-block-image"><img alt=""/></figure>
-				<!-- /wp:image -->
+<!-- wp:paragraph {"fontSize":"base"} -->
+<p class="has-base-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel ante nec diam placerat vulputate. Fusce ut dui sagittis lacus rutrum tristique ut vitae leo. Phasellus ultrices nunc nec tempus condimentum. Sed facilisis scelerisque lobortis. Suspendisse potenti. Duis iaculis at mi eget porttitor. Donec elementum lacus ut fermentum aliquet.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
 
-				<!-- wp:heading {"level":3} -->
-				<h3 class="wp-block-heading">Card Title</h3>
-				<!-- /wp:heading -->
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"className":"ucsc__card ucsc__card-plain","layout":{"type":"default"}} -->
+<div class="wp-block-group ucsc__card ucsc__card-plain"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
 
-				<!-- wp:paragraph {"fontSize":"base"} -->
-				<p class="has-base-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel ante nec diam placerat vulputate. Fusce ut dui sagittis lacus rutrum tristique ut vitae leo. Phasellus ultrices nunc nec tempus condimentum. Sed facilisis scelerisque lobortis. Suspendisse potenti. Duis iaculis at mi eget porttitor. Donec elementum lacus ut fermentum aliquet.</p>
-				<!-- /wp:paragraph --></div>
-			<!-- /wp:group --></div>
-		<!-- /wp:column -->
+<!-- wp:paragraph {"className":"ucsc__card-overline","fontSize":"small"} -->
+<p class="ucsc__card-overline has-small-font-size">Overline</p>
+<!-- /wp:paragraph -->
 
-		<!-- wp:column -->
-		<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"className":"ucsc__card ucsc__card-plain","layout":{"type":"default"}} -->
-			<div class="wp-block-group ucsc__card ucsc__card-plain"><!-- wp:image -->
-				<figure class="wp-block-image"><img alt=""/></figure>
-				<!-- /wp:image -->
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Card Title</h3>
+<!-- /wp:heading -->
 
-				<!-- wp:heading {"level":3} -->
-				<h3 class="wp-block-heading">Card Title</h3>
-				<!-- /wp:heading -->
+<!-- wp:paragraph {"fontSize":"base"} -->
+<p class="has-base-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel ante nec diam placerat vulputate. Fusce ut dui sagittis lacus rutrum tristique ut vitae leo. Phasellus ultrices nunc nec tempus condimentum. Sed facilisis scelerisque lobortis. Suspendisse potenti. Duis iaculis at mi eget porttitor. Donec elementum lacus ut fermentum aliquet.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
 
-				<!-- wp:paragraph {"fontSize":"base"} -->
-				<p class="has-base-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel ante nec diam placerat vulputate. Fusce ut dui sagittis lacus rutrum tristique ut vitae leo. Phasellus ultrices nunc nec tempus condimentum. Sed facilisis scelerisque lobortis. Suspendisse potenti. Duis iaculis at mi eget porttitor. Donec elementum lacus ut fermentum aliquet.</p>
-				<!-- /wp:paragraph --></div>
-			<!-- /wp:group --></div>
-		<!-- /wp:column --></div>
-	<!-- /wp:columns --></div>
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"className":"ucsc__card ucsc__card-plain","layout":{"type":"default"}} -->
+<div class="wp-block-group ucsc__card ucsc__card-plain"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"className":"ucsc__card-overline","fontSize":"small"} -->
+<p class="ucsc__card-overline has-small-font-size">Overline</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Card Title</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"base"} -->
+<p class="has-base-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel ante nec diam placerat vulputate. Fusce ut dui sagittis lacus rutrum tristique ut vitae leo. Phasellus ultrices nunc nec tempus condimentum. Sed facilisis scelerisque lobortis. Suspendisse potenti. Duis iaculis at mi eget porttitor. Donec elementum lacus ut fermentum aliquet.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
 <!-- /wp:group -->
