cde835d45871d116fb9c230c198d692cee293e4e
Rob Knight <raknight@ucsc.edu>
2023-10-03 18:07:15 -0700

    fix: 🐛 Remove pesky core pattern that stuck around

--- lib/blocks.php
+++ lib/blocks.php
@@ -4,6 +4,9 @@ add_action( 'init', function () {
 	// Register Main Nav Block
 	register_block_type( get_template_directory() . '/blocks/main-nav' );
 
+	// Remove core pattern
+	unregister_block_pattern( 'core/social-links-shared-background-color' );
+
 	// Register Block Categories
 	register_block_pattern_category(
 		'page_layout',
