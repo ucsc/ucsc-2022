1556f973116a89e68474a87e3f9d818952c7770c
Rob Knight <raknight@ucsc.edu>
2023-10-04 07:53:16 -0700

    refactor: 📝 Rename and add padding to page with left navigation pattern

--- patterns/page-left-navigation.php
+++ patterns/page-left-navigation.php
@@ -1,15 +1,15 @@
 <?php
  /**
-  * Title: Left navigation
-  * Slug: ucsc-2022/sidebar-with-navigation
+  * Title: Page with left navigation
+  * Slug: ucsc-2022/page-left-navigation
   * Categories: page_layout
   */
 ?>
 
-<!-- wp:columns -->
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"left":"var:preset|spacing|20"}}}} -->
 <div class="wp-block-columns"><!-- wp:column {"width":"33.33%"} -->
 <div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:heading {"level":3,"fontSize":"base"} -->
-<h3 class="wp-block-heading has-base-font-size"><strong>Navigation header</strong></h3>
+<h3 class="wp-block-heading has-base-font-size"><strong>Sidebar header</strong></h3>
 <!-- /wp:heading -->
 
 <!-- wp:navigation {"ref":2910,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} /--></div>
