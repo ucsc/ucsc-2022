1439d1676f5cc890bced56610715ecd7527384d5
Rob Knight <raknight@ucsc.edu>
2023-10-04 17:10:28 -0700

    Revert "refactor: 📝 Add className to left navigation page layout"
    This reverts commit 8d937e72159c6084153c9bb6ae08d21236260223.

--- patterns/page-left-navigation.php
+++ patterns/page-left-navigation.php
@@ -6,16 +6,16 @@
   */
 ?>
 
-<!-- wp:columns {"style":{"spacing":{"blockGap":{"left":"var:preset|spacing|20"}}},"className":"ucsc-page-left-navigation"} -->
-<div class="wp-block-columns ucsc-page-left-navigation"><!-- wp:column {"width":"66.66%"} -->
-<div class="wp-block-column" style="flex-basis:66.66%"></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"width":"33.33%"} -->
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"left":"var:preset|spacing|20"}}}} -->
+<div class="wp-block-columns"><!-- wp:column {"width":"33.33%"} -->
 <div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:heading {"level":3,"fontSize":"base"} -->
 <h3 class="wp-block-heading has-base-font-size"><strong>Sidebar header</strong></h3>
 <!-- /wp:heading -->
 
 <!-- wp:navigation {"ref":2910,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
