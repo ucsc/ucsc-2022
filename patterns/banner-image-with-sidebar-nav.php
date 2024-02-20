da84960de960b9a84bd6f62ec920be1e42102ad7
Jason Chafin <jchafin@ucsc.edu>
2024-02-20 08:57:34 -0800

    Initial draft of banner with sidebar nav pattern #151

---
+++ patterns/banner-image-with-sidebar-nav.php
@@ -0,0 +1,44 @@
+<?php
+ /**
+  * Title: Banner image with sidebar navigation
+  * Slug: ucsc-2022/banner-image-with-sidebar-nav
+  * Categories: page_layout
+  */
+?>
+<!-- wp:group {"align":"full","className":"ucsc-page-header","layout":{"type":"default"},"fontSize":"base"} -->
+<div class="wp-block-group alignfull ucsc-page-header has-base-font-size"><!-- wp:cover {"url":"https://unsplash.com/photos/a562ZEFKW8I/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NDV8fGxhbmRzY2FwZSUyMG1pbmltYWxpc3R8ZW58MHx8fHwxNjk2MzgwNTc4fDA\u0026w=1920","dimRatio":10,"focalPoint":{"x":0.91,"y":1},"contentPosition":"center center","isDark":false,"style":{"spacing":{"padding":{"top":"var:preset|spacing|50"}}},"className":"ucsc-page-header__content"} -->
+<div class="wp-block-cover is-light ucsc-page-header__content" style="padding-top:var(--wp--preset--spacing--50)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-10 has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="https://unsplash.com/photos/a562ZEFKW8I/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NDV8fGxhbmRzY2FwZSUyMG1pbmltYWxpc3R8ZW58MHx8fHwxNjk2MzgwNTc4fDA&amp;w=1920" style="object-position:91% 100%" data-object-fit="cover" data-object-position="91% 100%"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:post-title {"level":1,"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"backgroundColor":"ucsc-primary-yellow","textColor":"ucsc-primary-blue","className":"ucsc-page-header__title primary-post-title","fontSize":"seven"} /--></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:columns {"className":"ucsc__sidebar-with-navigation"} -->
+<div class="wp-block-columns ucsc__sidebar-with-navigation"><!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading">Heading</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec mollis. Quisque convallis libero in sapien pharetra tincidunt. Aliquam elit ante, malesuada id, tempor eu, gravida id, odio. Maecenas suscipit, risus et eleifend imperdiet, nisi orci ullamcorper massa, et adipiscing orci velit quis magna. Praesent sit amet ligula id orci venenatis auctor. Phasellus porttitor, metus non tincidunt dapibus, orci pede pretium neque, sit amet adipiscing ipsum lectus et libero. Aenean bibendum. Curabitur mattis quam id urna. Vivamus dui. Donec nonummy lacinia lorem. Cras risus arcu, sodales ac, ultrices ac, mollis quis, justo. Sed a libero. Quisque risus erat, posuere at, tristique non, lacinia quis, eros.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Cras volutpat, lacus quis semper pharetra, nisi enim dignissim est, et sollicitudin quam ipsum vel mi. Sed commodo urna ac urna. Nullam eu tortor. Curabitur sodales scelerisque magna. Donec ultricies tristique pede. Nullam libero. Nam sollicitudin felis vel metus. Nullam posuere molestie metus. Nullam molestie, nunc id suscipit rhoncus, felis mi vulputate lacus, a ultrices tortor dolor eget augue. Aenean ultricies felis ut turpis. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse placerat tellus ac nulla. Proin adipiscing sem ac risus. Maecenas nisi. Cras semper.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Praesent interdum mollis neque. In egestas nulla eget pede. Integer eu purus sed diam dictum scelerisque. Morbi cursus velit et felis. Maecenas faucibus aliquet erat. In aliquet rhoncus tellus. Integer auctor nibh a nunc fringilla tempus. Cras turpis urna, dignissim vel, suscipit pulvinar, rutrum quis, sem. Ut lobortis convallis dui. Sed nonummy orci a justo. Morbi nec diam eget eros eleifend tincidunt.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:heading {"level":3,"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"base"} -->
+<h3 class="wp-block-heading has-base-font-size" style="text-transform:uppercase">Navigation header</h3>
+<!-- /wp:heading -->
+
+<!-- wp:navigation {"ref":2867,"textColor":"ucsc-royal-blue","overlayMenu":"never","overlayTextColor":"ucsc-royal-blue","maxNestingLevel":1,"layout":{"type":"flex","orientation":"vertical","justifyContent":"left"},"fontSize":"base"} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
