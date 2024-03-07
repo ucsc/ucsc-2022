f627a94735cafe5fa198d51de9348da182a12964
Jason Chafin <Herm71@users.noreply.github.com>
2024-03-06 16:12:51 -0800

    Add "last modified" date to the region below the site footer (#298)
    * feat: :sparkles: Add "Last modified:" date to footer below Truss #160
    * refactor: :art: Rework "Last modified" date. Re-factor html, new CSS, additonal function. Issue #160
    * Add padding around outer `<div>`, adjust media query to match Truss. #160

--- functions.php
+++ functions.php
@@ -365,13 +365,20 @@ add_action( 'wp_footer', 'ucsc_truss_assets' );
 
 function ucsc_truss_assets() { ?>
 
-<!-- Script and style to include our components library, Truss.  -->
-<script type="module" src="https://unpkg.com/@ucsantacruz/truss@0.7.11/dist/ucsc-trss/ucsc-trss.esm.js"></script>
-<link rel="stylesheet" href="https://unpkg.com/@ucsantacruz/truss@0.7.11/dist/ucsc-trss/ucsc-trss.css">
-
-<?php
+	<!-- Script and style to include our components library, Truss.  -->
+	<script type="module" src="https://unpkg.com/@ucsantacruz/truss@0.7.11/dist/ucsc-trss/ucsc-trss.esm.js"></script>
+	<link rel="stylesheet" href="https://unpkg.com/@ucsantacruz/truss@0.7.11/dist/ucsc-trss/ucsc-trss.css">
+	<?php
 }
 
+add_action( 'wp_footer', 'ucsc_last_modified', 10 );
+
+function ucsc_last_modified() {
+	echo '<!-- Last modified code.  --><div class="last-modified__outer" ><div class="last-modified__inner"><div class="last-modified">Last modified: ';
+	echo do_shortcode('[last-modified]');
+	echo '</div></div></div><!-- End last modified code.  -->';
+
+}
 /**
  * Add Excerpts to Pages
  */
