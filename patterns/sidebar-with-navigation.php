efbe20e1e1c749b2f8f3e3163a25c473ebc65299
Rob Knight <raknight@ucsc.edu>
2024-04-26 13:17:26 -0700

    fix(pattern): Adjust spacing in and between columns in the `sidebar with navigation` pattern Fixes #319

--- patterns/sidebar-with-navigation.php
+++ patterns/sidebar-with-navigation.php
@@ -5,10 +5,10 @@
   * Categories: page_layout
   */
 ?>
-<!-- wp:columns {"className":"ucsc__sidebar-with-navigation"} -->
-<div class="wp-block-columns ucsc__sidebar-with-navigation"><!-- wp:column {"width":"33.33%"} -->
-<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:heading {"level":3,"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"base"} -->
-<h3 class="wp-block-heading has-base-font-size" style="text-transform:uppercase">Navigation header</h3>
+<!-- wp:columns {"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"className":"ucsc__sidebar-with-navigation"} -->
+<div class="wp-block-columns ucsc__sidebar-with-navigation" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--30)"><!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:heading {"level":3,"style":{"typography":{"textTransform":"uppercase"},"spacing":{"margin":{"bottom":"var:preset|spacing|30"}}},"fontSize":"base"} -->
+<h3 class="wp-block-heading has-base-font-size" style="margin-bottom:var(--wp--preset--spacing--30);text-transform:uppercase">Navigation header</h3>
 <!-- /wp:heading -->
 
 <!-- wp:navigation {"textColor":"ucsc-royal-blue","overlayMenu":"never","overlayTextColor":"ucsc-royal-blue","maxNestingLevel":1,"layout":{"type":"flex","orientation":"vertical","justifyContent":"left"},"fontSize":"base"} /--></div>
