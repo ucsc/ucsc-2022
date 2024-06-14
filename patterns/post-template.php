37eec608ee63c1e197e7f9d435d78ef36c59e183
Jason Chafin <jchafin@ucsc.edu>
2024-06-14 10:16:57 -0700

    Initial  scaffolding of post template pattern.

---
+++ patterns/post-template.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: UCSC Post Template
+ * Slug: ucsc-2022/query-loop
+ * Block Types: core/post-template core/post-title, core/featured-image, core/post-date, core/post-excerpt
+ * Categories: text_layout
+ */
+?>
+<!-- wp:group -->
+<div class="wp-block-query"><!-- wp:post-template {"layout":{"type":"grid","columnCount":3}} -->
+<!-- wp:group {"className":"ucsc__card ucsc__card\u002d\u002dquery-loop","layout":{"type":"default"}} -->
+<div class="wp-block-group ucsc__card ucsc__card--query-loop"><!-- wp:post-featured-image {"isLink":true,"width":"","height":"","sizeSlug":"sixteen-nine","className":"ucsc-query-loop__image"} /-->
+
+<!-- wp:post-terms {"term":"category","style":{"elements":{"link":{"color":{"text":"var:preset|color|ucsc-secondary-blue"},":hover":{"color":{"text":"var:preset|color|ucsc-primary-blue"}}}}},"textColor":"black","className":"ucsc-query-loop__category","fontSize":"small"} /-->
+
+<!-- wp:post-title {"isLink":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|ucsc-secondary-blue"},":hover":{"color":{"text":"var:preset|color|ucsc-primary-blue"}}}}},"fontSize":"three"} /-->
+
+<!-- wp:post-date {"style":{"spacing":{"margin":{"bottom":"0"}}},"textColor":"black","className":"ucsc-query-loop__post-date","fontSize":"base"} /-->
+
+<!-- wp:post-excerpt {"className":"ucsc-query-loop__excerpt"} /--></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></div>
+<!-- /wp:group -->
