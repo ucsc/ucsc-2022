75aba91edf1ee86ae06a8c6426759c20def577e4
Jason Chafin <1000543+Herm71@users.noreply.github.com>
2025-09-17 17:08:42 -0700

    fix: :package: Edits to theme updater

--- wp-theme-version-updater.js
+++ wp-theme-version-updater.js
@@ -1,7 +1,11 @@
 module.exports.readVersion = function (contents) {
 	const capturingRegex = /Version: (?<vnum>[0-9]+\.[0-9]+\.[0-9]+)/;
 	const found = contents.match(capturingRegex);
-	return found.groups.vnum;
+	if (found && found.groups && found.groups.vnum) {
+		return found.groups.vnum;
+	} else {
+		return null; // or throw new Error('Version not found');
+	}
 };
 
 module.exports.writeVersion = function (_contents, version) {
